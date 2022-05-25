9dc3bb8b42255f82fefdadeec395ae23746e94c1
NelsonCardoso01 <102222338+NelsonCardoso01@users.noreply.github.com>
2022-05-25 10:49:46 +0100

    segunda tentativa

--- sketch.js
+++ sketch.js
@@ -1,139 +1,213 @@
 var PLAY = 1;
 var END = 0;
 var gameState = PLAY;
+var bg;
+var bgImage;
 
-var ground,ground_image,invisible_ground;
-var boneco_baixo;
-var obstaclesGroup,obstacle1;
-var jumpSound,dieSound,checkpointSound;
+var player, player_running,player_collided;
+var ground, invisibleGround, groundImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
+
 var score;
-var gameOver,restart,gameOverImage,restartImage;
+var gameOverImg,restartImg
+var jumpSound , checkPointSound, dieSound
 
 function preload(){
-     // preload() runs once
-    
-  ground_image=loadImage("contents/fundo.png");
-  boneco_baixo=loadImage("contents/boneco_baixo.png");
-  obstacle1=loadImage("contents/espinho_baixo.png");
-  jumpSound = loadSound("contents/jump.mp3")
-  dieSound = loadSound("contents/die.mp3")
-  checkPointSound = loadSound("contents/checkPoint.mp3")
-  gameOverImage=loadImage("contents/gameOver1.png");
-  restartImage=loadImage("contents/restart1.png");
+  player_running = loadAnimation("r1.png","r2.png","r3.png","r4.png","r5.png","r6.png","r7.png","r8.png");
+  playercollided = loadImage("r1.png");
+  bgImage = loadImage("bg.png")
+  groundImage = loadImage("race.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("h1.png");
+  obstacle2 = loadImage("h2.png");
+  obstacle3 = loadImage("h3.png");
+  obstacle4 = loadImage("h4.png");
+  obstacle5 = loadImage("h2.png");
+  obstacle6 = loadImage("h3.png");
+  
+  restartImg = loadImage("res.png")
+  gameOverImg = loadImage("go.png")
+  
+  jumpSound = loadSound("jump.mp3")
+  dieSound = loadSound("die.mp3")
+  checkPointSound = loadSound("checkPoint.mp3")
 }
 
 function setup() {
- createCanvas(600,500);
+  createCanvas(600, 200);
+
+  var message = "This is a message";
+ console.log(message)
+  bg = createSprite(300,130,600,200);
+  player = createSprite(50,160,20,50);
+  player.addAnimation("running", player_running);
+  player.addAnimation("collided", player_collided);
+  bg.addImage("bg",bgImage)
+  bg.scale = 0.7
+
+  player.scale = 0.2;
   
-ground=createSprite(0,0,0,0);
-  ground.shapeColor="white";
-ground.addImage("fundo",ground_image);
-ground.scale=1.4;
-   ground.velocityX=-1
+  ground = createSprite(200,300,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
   
-   boneco_baixo=createSprite(300,420,600,10);
-  boneco_baixo.addAnimation("boneco_baixo",boneco_baixo);
-  boneco_baixo.addImage("boneco_baixo",boneco_baixo);
-  boneco_baixo.scale=0.2;
- // girl.velocityX=2;
-  boneco_baixo.debug=false;
-  boneco_baixo.setCollider("rectangle",0,0,boneco_baixo.width,boneco_baixo.height);
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
   
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
   
-  invisible_ground=createSprite(300,470,600,10);
-  invisible_ground.visible=false;
+ 
+  gameOver.scale = 0.4;
+  restart.scale = 0.08;
   
-   gameOver = createSprite(300,100);
-  gameOver.addImage(gameOverImage);
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
   
-  restart = createSprite(300,180);
-  restart.addImage(restartImage);
+  obstaclesGroup = createGroup();
+  cloudsGroup = createGroup();
+
   
-  obstaclesGroup=new Group();
+  player.setCollider("rectangle",0,0,100,300);
   
-  score=0;
+  score = 0;
+  
 }
 
 function draw() {
- background("contents/fundo.png");
   
- // console.log(girl.y);
-   //Gravity
-boneco_baixo.velocityY = boneco_baixo.velocityY + 0.8;
-boneco_baixo.collide(invisible_ground); 
-  
-  
-   if (gameState===PLAY){
-    gameOver.visible=false;
-  restart.visible=false;
-    //  zombie.y=girl.y;
-   score = score + Math.round(getFrameRate()/60);
- 
+  background("white");
+  fill("red")
+  text("Score: "+ score, 500,10);
 
- ground.velocityX = -(4 + 3* score/100);
-     
-   if (ground.x < 0){
-      ground.x = ground.width/2;
-    }
-  
-     if(score>0 && score%100 === 0){
+  if(gameState === PLAY){
+
+    gameOver.visible = false;
+    restart.visible = false;
+    
+    ground.velocityX = -8 ;
+    score = score + Math.round(frameRate()/30);
+    
+    if(score>0 && score%100 === 0){
        checkPointSound.play() 
     }
     
- if((keyDown("space")&& boneco_baixo.y >= 220)) {
-   boneco_baixo.velocityY = -12;
-    jumpSound.play();
-  }  
-  
-  if (boneco_baixo.isTouching(obstaclesGroup)){
-    gameState=END;
-     dieSound.play();
-  }
-  }
-else if ( gameState===END) {
-  gameOver.visible=true;
-  restart.visible=true;
-  ground.velocityX = 0;
-     boneco_baixo.velocityY = 0
-     boneco_baixo.changeImage("boneco_baixo",boneco_baixo);
-      //set lifetime of the game objects so that they are never destroyed
-    obstaclesGroup.setLifetimeEach(-1);
-   obstaclesGroup.setVelocityXEach(0);
-  
-    if(mousePressedOver(restart)) {
-      reset();
+    if (ground.x < 0){
+      ground.x = ground.width/2;
     }
-} 
+    player.depth = ground.depth+1
+    
+    if(keyDown("space")&& player.y >= 130) {
+        player.velocityY = -8;
+        jumpSound.play();
+    }
+    
+    player.velocityY =player.velocityY + 0.8
+  
+    spawnClouds();
+  
+    spawnObstacles();
+    
+    if(obstaclesGroup.isTouching(player)){
+        jumpSound.play();
+        gameState = END;
+        dieSound.play()
+      
+    }
+  }
+   else if (gameState === END) {
+      gameOver.visible = true;
+      restart.visible = true;
+     
+      player.changeAnimation("collided", playercollided);
+    
+     player.x = player.x+5;
+     player.y = player.y + 5
+  
+     
+      ground.velocityX = 0;
+      player.velocityY = 0
+      
+     
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+     
+     obstaclesGroup.setVelocityXEach(0);
+     cloudsGroup.setVelocityXEach(0);    
+   }
   
  
+  player.collide(invisibleGround);
+  
+  if(mousePressedOver(restart)&&gameState === END) {
+      gameState = PLAY
+     obstaclesGroup.setLifetimeEach(0);
+    cloudsGroup.setLifetimeEach(0);
+    score = 0;
+    player.x = 50;
+    player.y = 160;
+     player.changeAnimation("running", player_running);
+
+    }
+
+
   drawSprites();
-  fill("lightpink");
-  textSize(20);
-   text("Score: "+ score, 500,50);
 }
 
 function reset(){
-  gameState=PLAY;
-gameOver.visible=false;
-restart.visible=false;
-boneco_baixo.changeAnimation("boneco_baixo",boneco_baixo);
-  obstaclesGroup.destroyEach();
-  score=0;
+  
+
 }
 
-function spawnObstacles() {
-   if (frameCount % 60 === 0){
-   var obstacle = createSprite(600,450,10,40);
-   obstacle.velocityX = -6 ;//+ score/100);
+
+function spawnObstacles(){
+ if (frameCount % 50 === 0){
+   var obstacle = createSprite(600,170,10,40);
+   obstacle.velocityX = -8;
    
     //generate random obstacles
-   var rand = Math.round(random(1,6));
-     obstacle.addImage(obstacle1);
-   obstacle.scale=0.1;
-      obstaclesGroup.add(obstacle);
-    obstacle.debug=false;
-obstacle.setCollider("circle",0,0,1);
-   }
-     
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+   
+    obstacle.scale = 0.1;
+    obstacle.lifetime = 300;
+   obstacle.setCollider("rectangle",0,0,200,400)
+    obstaclesGroup.add(obstacle);
+ }
+}
+
+function spawnClouds() {
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(600,120,40,10);
+    cloud.y = Math.round(random(80,120));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+    cloud.lifetime = 200;
+    
+    cloud.depth = player.depth;
+    player.depth = player.depth + 1;
+    
+    cloudsGroup.add(cloud);
+  }
 }
 
